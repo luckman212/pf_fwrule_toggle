940529178e52025394704c63e3f38e25dcac9368
luckman212 <1992842+luckman212@users.noreply.github.com>
2022-08-28 16:52:57 -0400

    added multi-rule processing and actions based on rule descriptions

--- README.md
+++ README.md
@@ -2,39 +2,69 @@
 
 Helper script for pfSense that lists rules and toggles them on/off via CLI. The script lists and operates on both `nat` and `filter` (standard) rule types.
 
-# setup
+# Setup
 
 - copy the php script to your firewall with scp or whatever other method suits you
 - run it using the syntax below
-- you need to supply a rule id as a parameter, or `-l` to list all rules
-- prefixing the ruleID with `n` e.g. `n3` will operate on NAT rule #3
-- optionally add a 0 or 1 to explicitly set a rule on/off
-- I added this functionality. 
-- otherwise, rule status will be toggled
-
-# example usage
-
-list rules
+- you need to supply one or more rule ids as a parameter, or `-l` to list all rules
+- prefixing the ruleIDs with `n` e.g. `n3` will operate on NAT rule #3
+- optionally set the action to `enable`, `disable`, or `toggle` (default=`toggle`)
+- if action is not specified, it defaults to `toggle`
+- you can use the following long args instead, if you prefer:
 ```
+-l or --list
+-d or --desc
+-h or --help
+```
+
+# Usage
+
+## Specify rules by ID
+
+This mode accepts 1 or more numeric rule IDs. You can prefix the number with `n` to select NAT rules, as opposed to filter rules which are the default.
+```sh
+php -q fwrule_toggle.php <ruleid>[,ruleid...] [action]
+```
+- `action` can be `enable`, `disable`, or `toggle`
+
+## Specify rules by Description
+
+This mode operates on the rule description. If multiple rules share an identical description, they will be operated on **as a group**.
+```sh
+php -q fwrule_toggle.php -d <rule_desc> [type] [action]
+```
+- `type` can be `nat` or `filter`—if omitted, defaults to filter
+- if the description contains spaces, you must surround it with quotes e.g. `'my fancy rule'`
+
+# Examples
+
+## list rules
+```sh
 php -q fwrule_toggle.php -l
 ```
 
-toggle filter (standard) rule 36
-```
+## toggle filter (standard) rule 36
+```sh
 php -q fwrule_toggle.php 36
 ```
 
-turn NAT rule 23 **on**
-```
-php -q fwrule_toggle.php n23 1
-```
-
-turn rule 5 **off**
-```
-php -q fwrule_toggle.php 5 0
+## turn NAT rule 23 **on**
+```sh
+php -q fwrule_toggle.php n23 enable
 ```
 
-# based on
+## turn rules 5,11 and 17 **off**
+```sh
+php -q fwrule_toggle.php 5,11,17 disable
+```
+
+## disable all filter rules named `external access`
+```sh
+php -q fwrule_toggle.php -d 'external access' filter disable
+```
+
+
+# Based on
 
 - https://www.reddit.com/r/PFSENSE/comments/usdlaf/anybody_have_a_shell_script_to_disableenable_a/
 - https://forum.netgate.com/topic/51063/enable-disable-existing-rule-via-script/
--- fwrule_toggle.php
+++ fwrule_toggle.php
@@ -7,57 +7,176 @@
 
 require_once("config.inc");
 require_once("filter.inc");
-global $config;
 parse_config(true);
+$eids = array();
+$dids = array();
+$iids = array();
 
-if ($argv[1] == '-l') {
-  $rules = $config['filter']['rule'];
-  $natrules = $config['nat']['rule'];
-  printf("[%s]\n", "Standard rules");
-  foreach ($rules as $id => $rule) {
-    $stat = (isset($rule['disabled']) ? '*' : ' ');
-    printf("%4d %s %s (%s)\n", $id, $stat, $rule['interface'], $rule['descr']);
-  }
-  printf("[%s] (%s)\n", "NAT rules", "iface/desc, *=disabled");
-  foreach ($natrules as $id => $rule) {
-    $stat = (isset($rule['disabled']) ? '*' : ' ');
-    printf("%4d %s %s (%s)\n", $id, $stat, $rule['interface'], $rule['descr']);
-  }
+function show_help() {
+  $fname = basename(__FILE__);
+  $helptext = sprintf('usage:' . "\n" .
+    '  php -q %1$s <ruleid>[,ruleid...] [enable|disable|toggle]' . "\n" .
+    '  php -q %1$s -d <rule_desc> <nat|filter> [enable|disable|toggle]' . "\n",
+    $fname
+  );
+  print($helptext);
   exit();
 }
 
-$id = $argv[1];
-if (strpos($id, 'n') === 0) {
-  $rtype = 'nat';
-  $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
-} else {
-  $rtype = 'filter';
-}
-if ( !(ctype_digit($id)) || intval($id) < 0 ) {
-  exit("specify a rule id (use e.g. `n3` for NAT rule) or -l to list rules\n");
-}
-$rule = &$config[$rtype]['rule'][$id];
-$force = $argv[2];
-if (isset($force)) {
-  $s = ($force ? 'enabled' : 'disabled');
-} else {
-  $s = (isset($rule['disabled']) ? 'enabled' : 'disabled');
+function rule_id_from_desc($desc, $type): array {
+  global $config;
+  $matched_ids = array();
+  if (strlen($type) == 0) {
+    $type = 'filter';
+  }  
+  printf("looking for %s rules named `%s`\n", $type, $desc);
+  $rules = $config[$type]['rule'];
+  foreach ($rules as $id => $rule) {
+    // printf("processing a %s rule, id:%s if:%s desc:%s\n", $type, $id, $rule['interface'], $rule['descr']);
+    if ($rule['descr'] == $desc) {
+      if ($type == 'nat') {
+        $matched_ids[] = sprintf("n%d", $id);
+      } else {
+        $matched_ids[] = $id;
+      }
+    }
+  }
+  return $matched_ids;
 }
 
-switch ($s) {
-  case 'enabled':
-    unset($rule['disabled']);
+function validate_rule_ids($rule_ids) {
+  foreach ($rule_ids as $id) {
+    $id_test = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+    if ( !(ctype_digit($id_test)) || intval($id_test) < 0 ) {
+      print("invalid rule id: {$id}\n");
+      return false;
+    }
+  }
+  return true;
+}
+
+function process_all_rules($rule_ids, $action) {
+  if (!validate_rule_ids($rule_ids)) {
+    exit("specify only comma-separated, numeric rule ids (prefix with `n` for NAT rules)\n");
+  }
+  foreach ($rule_ids as $id) {
+    process_rule($id, $action);
+  }
+}
+
+function process_rule($oid, $action) {
+  global $config, $eids, $dids, $iids;
+  $oid = trim($oid);
+  if (empty($oid)) {
+    return false;
+  }
+  if (!isset($action)) {
+    $action = 'toggle';
+  }
+  if (strpos($oid, 'n') === 0) {
+    $rtype = 'nat';
+  } else {
+    $rtype = 'filter';
+  }
+  $id = filter_var($oid, FILTER_SANITIZE_NUMBER_INT);
+  $rule = &$config[$rtype]['rule'][$id];
+  if (!isset($rule)) {
+    printf("rule %s does not exist on this system\n", $oid);
+    return false;
+  }
+  if ($action == 'toggle') {
+    $action = (isset($rule['disabled']) ? 'enable' : 'disable');
+  }
+  switch ($action) {
+    case 'enable':
+      if (isset($rule['disabled'])) {
+        unset($rule['disabled']);
+        $eids[] = $oid;
+      }
+      break;
+    case 'disable':
+      if (!isset($rule['disabled'])) {
+        $rule['disabled'] = true;
+        $dids[] = $oid;
+      }
+      break;
+    default:
+      $err = "(invalid action)";
+      $iids[] = $oid;
+      break;
+  }
+  printf("%s rule %s: %s %s\n", $rtype, $oid, $action, $err);
+}
+
+switch ($argv[1]) {
+  case '-d':
+  case '--desc':
+    $desc = trim($argv[2]);
+    if (empty($desc)) {
+      print("description cannot be blank\n");
+      show_help();
+    }
+    $rule_ids = rule_id_from_desc($desc, $argv[3]);
+    $action = $argv[4];
+    if (count($rule_ids) > 0) {
+      printf("matched ids: %s\n", implode(',', $rule_ids));
+      process_all_rules($rule_ids, $argv[4]);
+    } else {
+      print("no matching rules found\n");
+    }
     break;
-  case 'disabled':
-    $rule['disabled'] = true;
-    break;
+  case '-l':
+  case '--list':
+    $rules = $config['filter']['rule'];
+    $natrules = $config['nat']['rule'];
+    if (count($rules) > 0) {
+      printf("[%s]\n", "Standard (filter) rules, *=disabled");
+      foreach ($rules as $id => $rule) {
+        $stat = (isset($rule['disabled']) ? '*' : ' ');
+        printf("%4d %s %s (%s)\n", $id, $stat, $rule['interface'], $rule['descr']);
+      }
+    }
+    if (count($natrules) > 0) {
+      printf("[%s] (%s)\n", "NAT rules", "iface/desc, *=disabled");
+      foreach ($natrules as $id => $rule) {
+        $stat = (isset($rule['disabled']) ? '*' : ' ');
+        printf("%4d %s %s (%s)\n", $id, $stat, $rule['interface'], $rule['descr']);
+      }
+    }
+    exit();
+  case '-h':
+  case '--help':
+    show_help();
   default:
-    print('unknown');
+    if (empty($argv[1])) {
+      show_help();
+    }
+    $rule_ids = explode(',', $argv[1]);
+    $action = $argv[2];
+    process_all_rules($rule_ids, $action);
+    break;
 }
 
-$msg = "$rtype rule $id: $s";
-write_config($msg);
-filter_configure();
+if ((count($eids) + count($dids)) > 0) {
+  $msg = "ruleset updated,";
+  if (count($eids)>0) {
+    $msg .= sprintf(" enabled:[%s]", implode(',', $eids));
+  }
+  if (count($dids)>0) {
+    $msg .= sprintf(" disabled:[%s]", implode(',', $dids));
+  }
+  if (count($iids)>0) {
+    $msg .= sprintf(" ignored:[%s]", implode(',', $iids));
+  }
+  filter_configure();
+  write_config($msg);
+} else {
+  $msg = sprintf("script called but no changes were made to the ruleset, args: [%s]",
+    implode('|', array_slice($argv, 1))
+  );
+  log_error($msg);
+}
+
 print("$msg\n");
 
 ?>
